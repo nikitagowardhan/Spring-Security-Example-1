932ef4f0228790534ba3bc30e29138f7a9aa47e4
nikitagowardhan <nikitagowar02@gmail.com>
2024-05-04 14:43:31 +0530

    Spring Security adding dependencies

--- src/main/java/asteric/in/Day2/controller/MyController.java
+++ src/main/java/asteric/in/Day2/controller/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyController {
 	
 	
-	@RequestMapping
+	@RequestMapping("/test1")
 	String MyFun()
 	{
 		return "home.jsp";
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-Welcome to JSP Page!....
+Welcome to JSP Page!
 </body>
 </html>
